9310170f27eb50edd84f4d6ba571bf7f1d6de253
DESKTOP-PS6SN4L\gobri <gobrisebane@gmail.com>
2025-12-16 19:23:03 +0900

    원본_오리지널

--- FlagTip/Program.cs
+++ FlagTip/Program.cs
@@ -20,7 +20,6 @@ namespace FlagTip
 
         private static IntPtr _hookID = IntPtr.Zero;
         private static IndicatorForm indicatorForm;
-        private static LowLevelMouseProc _mouseProc; // 대리자 보존
 
         // -------------------- Main --------------------
         [STAThread]
@@ -30,20 +29,25 @@ namespace FlagTip
             indicatorForm = new IndicatorForm();
 
 
+
+
             var caret = new Caret(indicatorForm);
 
-            // 폼 핸들 강제 생성 (옵션)
-            var h = indicatorForm.Handle;
+            //caret.show();
 
 
-            //var tracker = new CaretTracker(indicatorForm, caret);
+            //var tracker = new CaretTracker(indicatorForm);
             //tracker.Start();
 
 
-            _mouseProc = (nCode, wParam, lParam) =>
-                MouseHookCallback(nCode, wParam, lParam, _hookID, caret);
 
-            _hookID = SetMouseHook(_mouseProc);
+
+            _hookID = SetMouseHook((nCode, wParam, lParam) =>
+                MouseHookCallback(nCode, wParam, lParam, _hookID, caret)
+            );
+
+
+
 
             Application.Run(indicatorForm);
 
